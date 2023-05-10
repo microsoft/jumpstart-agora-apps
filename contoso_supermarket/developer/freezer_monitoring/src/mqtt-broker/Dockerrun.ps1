56b8c4a7cd3755f31e54f53ef92795c0565874cf
Chris Harris <charris@microsoft.com>
2023-05-10 09:10:06 -0700

    remove some unneeded files and references

--- contoso_supermarket/developer/freezer_monitoring/src/mqtt-broker/Dockerrun.ps1
+++ contoso_supermarket/developer/freezer_monitoring/src/mqtt-broker/Dockerrun.ps1
@@ -7,7 +7,4 @@ docker run --name $image -d `
     --hostname=mqtt-broker `
     -p 1883:1883 `
     js/$image 
-#     /bin/sh -c "/usr/sbin/mosquitto -c /mosquitto.conf; sleep 6000"
-#     /bin/sh -c "./entrypoint.sh; sleep 6000"; `
-# docker exec -it $image bash
 
--- contoso_supermarket/developer/freezer_monitoring/src/mqtt-simulator/Dockerrun.ps1
+++ contoso_supermarket/developer/freezer_monitoring/src/mqtt-simulator/Dockerrun.ps1
@@ -5,5 +5,3 @@ docker rm -f $image;
 
 docker run --name $image -d `
     js/$image
-# /bin/bash -c "./entrypoint.sh; sleep 6000";
-# docker exec -it $image bash
