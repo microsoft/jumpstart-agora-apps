d8b7d7cf89aafe63fa21fabbdc9af100426c59ca
Armando Blanco <armando.blanco@microsoft.com>
2024-03-08 11:58:46 -0500

    use the video from github in the RTSP simulator

--- contoso_supermarket/developer/data_emulator/src/obj/Debug/net6.0/DataEmulator.AssemblyInfo.cs
+++ contoso_supermarket/developer/data_emulator/src/obj/Debug/net6.0/DataEmulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataEmulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92eb18ff10673cb2e12208a2e39907c2ee5d7e51")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataEmulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataEmulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
