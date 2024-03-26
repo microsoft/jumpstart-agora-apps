fa8f4583b8573c60acead280424acf7f2885da77
Armando Blanco <armando.blanco@microsoft.com>
2024-03-26 17:11:52 -0400

    modifications to include the assembly line in the MQTT simulator

--- contoso_manufacturing/developer/decode-svc/build.sh
+++ contoso_manufacturing/developer/decode-svc/build.sh
@@ -19,3 +19,5 @@ docker push $ACR/$CONTAINER
 #az acr build --image  $ACR/$CONTAINER --registry $REGISTRY --file Dockerfile .
 
 # sudo docker run -e rtsp_url="rtsp://10.211.55.5:8554/stream" -e save_path="frames" -p 80:80 -it agoraarmbladev.azurecr.io/decode-svc:1.1
+Test the container
+docker run -e rtsp_url="rtsp://127.0.0.1:8554/stream" -e save_path="frames" -p 80:80 -it agoraarmbladev.azurecr.io/decode-svc:1.6
