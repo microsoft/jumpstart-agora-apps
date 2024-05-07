8f83f92ff5c556e491386abcb41b1f19e083152b
Francisco Cabrera <fcabrera@microsoft.com>
2024-05-07 12:11:30 -0400

    Fix merging conflicts

--- contoso_manufacturing/developer/webapp-decode/app.py
+++ contoso_manufacturing/developer/webapp-decode/app.py
@@ -11,15 +11,9 @@ app = Flask(__name__)
 
 camera = None  # Initialized later based on the selected video
 latest_choice_detector = None # Global variable to keep track of the latest choice of the user
-<<<<<<< HEAD
-ovms_url = os.environ.get('OVMS_URL', '192.168.0.4:31640')
-ai_metrics_iframe_url = os.environ.get('AI_METRICS_IFRAME_URL', 'http://localhost:5002')
-infra_metrics_iframe_url = os.environ.get('INFRA_METRICS_IFRAME_URL', 'http://localhost:5003')
-=======
-ovms_url = os.environ.get('OVMS_URL', '10.0.0.52:8080')
-influx_url = os.environ.get('INFLUX_URL', '10.0.0.52:8080')
-adx_url = os.environ.get('ADX_URL', '10.0.0.52:8080')
->>>>>>> 4628a86 (testing web app)
+ovms_url = os.environ.get('OVMS_URL', '')
+ai_metrics_iframe_url = os.environ.get('AI_METRICS_IFRAME_URL', '')
+infra_metrics_iframe_url = os.environ.get('INFRA_METRICS_IFRAME_URL', '')
 
 # Init the config.file.json
 with open('config_file.json') as config_file:
@@ -122,7 +116,6 @@ def show_iframe():
     return render_template('index.html', iframe_url=url)
 
 def gen_frames(video_name): 
-<<<<<<< HEAD
     """
     Generate frames from a video stream.
 
@@ -135,11 +128,6 @@ def gen_frames(video_name):
     Returns:
         None
     """
-=======
-    
-
-    
->>>>>>> 4628a86 (testing web app)
     global latest_choice_detector  
 
     if(latest_choice_detector is None or latest_choice_detector.model_name != video_name):
