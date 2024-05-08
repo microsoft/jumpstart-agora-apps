e7635d87dbbcff94a138dbeb59c68f0f9aa2b8b4
Francisco Cabrera <fcabrera@microsoft.com>
2024-05-07 23:03:43 -0400

    Fixes port and host

--- contoso_manufacturing/developer/webapp-decode/app.py
+++ contoso_manufacturing/developer/webapp-decode/app.py
@@ -216,9 +216,8 @@ def get_iframe_url():
 
     return Response(iframe_url, status=200)
 
-
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=False, host="0.0.0.0", port=5001)
 
 # Release the video capture object and close all windows
 camera.release()
